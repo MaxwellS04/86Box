339035c7f72f3abd2c9e2baf59cfe170fc055c52
MaxwellS04 <tongpauz@gmail.com>
2024-08-04 19:56:34 +0700

    Added the APC Hunter Industrial
    A rare ISA-only Socket 7 motherboard

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -10430,6 +10430,45 @@ const machine_t machines[] = {
 
     /* Socket 7 (Single Voltage) machines */
     /* 430FX */
+    {
+        .name = "[i430FX] APC Hunter Industrial",
+        .internal_name = "industrial",
+        .type = MACHINE_TYPE_SOCKET7_3V,
+        .chipset = MACHINE_CHIPSET_INTEL_430FX,
+        .init = machine_at_industrial_init,
+        .p1_handler = NULL,
+        .gpio_handler = NULL,
+        .available_flag = MACHINE_AVAILABLE,
+        .gpio_acpi_handler = NULL,
+        .cpu = {
+            .package = CPU_PKG_SOCKET5_7,
+            .block = CPU_BLOCK_NONE,
+            .min_bus = 50000000,
+            .max_bus = 66666667,
+            .min_voltage = 3380,
+            .max_voltage = 3520,
+            .min_multi = 1.5,
+            .max_multi = 3.0
+        },
+        .bus_flags = MACHINE_PS2,
+        .flags = MACHINE_IDE_DUAL | MACHINE_APM,
+        .ram = {
+            .min = 8192,
+            .max = 131072,
+            .step = 8192
+        },
+        .nvrmask = 127,
+        .kbc_device = NULL,
+        .kbc_p1 = 0xff,
+        .gpio = 0xffffffff,
+        .gpio_acpi = 0xffffffff,
+        .device = NULL,
+        .fdc_device = NULL,
+        .sio_device = NULL,
+        .vid_device = NULL,
+        .snd_device = NULL,
+        .net_device = NULL
+    },
     /* This has an AMIKey-2, which is an updated version of type 'H'.
        This also seems to be revision 2.1 with the FDC37C665 SIO. */
     {
