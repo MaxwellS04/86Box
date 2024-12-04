54a3237d5b6f6d8591923e22ba4ed3e4e34249a1
MaxwellS04 <tongpauz@gmail.com>
2024-12-04 11:11:11 +0700

    Machine table fixes

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -6497,45 +6497,6 @@ const machine_t machines[] = {
         .snd_device = NULL,
         .net_device = NULL
     },
-    {
-        .name = "[ALi M1429] ABit AB-AD4",
-        .internal_name = "abad4",
-        .type = MACHINE_TYPE_486,
-        .chipset = MACHINE_CHIPSET_ALI_M1429,
-        .init = machine_at_abad4_init,
-        .p1_handler = NULL,
-        .gpio_handler = NULL,
-        .available_flag = MACHINE_AVAILABLE,
-        .gpio_acpi_handler = NULL,
-        .cpu = {
-            .package = CPU_PKG_SOCKET1,
-            .block = CPU_BLOCK_NONE,
-            .min_bus = 0,
-            .max_bus = 0,
-            .min_voltage = 0,
-            .max_voltage = 0,
-            .min_multi = 0,
-            .max_multi = 0
-        },
-        .bus_flags = MACHINE_VLB,
-        .flags = MACHINE_APM,
-        .ram = {
-            .min = 1024,
-            .max = 32768,
-            .step = 1024
-        },
-        .nvrmask = 127,
-        .kbc_device = NULL,
-        .kbc_p1 = 0xff,
-        .gpio = 0xffffffff,
-        .gpio_acpi = 0xffffffff,
-        .device = NULL,
-        .fdc_device = NULL,
-        .sio_device = NULL,
-        .vid_device = NULL,
-        .snd_device = NULL,
-        .net_device = NULL
-    },
 	{
         .name = "[ALi M1409] ECS 8521",
         .internal_name = "8521",
@@ -6558,6 +6519,45 @@ const machine_t machines[] = {
             
         },
         .bus_flags = MACHINE_AT, 
+        .flags = MACHINE_APM,
+        .ram = {
+            .min = 1024,
+            .max = 32768,
+            .step = 1024
+        },
+        .nvrmask = 127,
+        .kbc_device = NULL,
+        .kbc_p1 = 0xff,
+        .gpio = 0xffffffff,
+        .gpio_acpi = 0xffffffff,
+        .device = NULL,
+        .fdc_device = NULL,
+        .sio_device = NULL,
+        .vid_device = NULL,
+        .snd_device = NULL,
+        .net_device = NULL
+    },
+    {
+        .name = "[ALi M1429] ABit AB-AD4",
+        .internal_name = "abad4",
+        .type = MACHINE_TYPE_486,
+        .chipset = MACHINE_CHIPSET_ALI_M1429,
+        .init = machine_at_abad4_init,
+        .p1_handler = NULL,
+        .gpio_handler = NULL,
+        .available_flag = MACHINE_AVAILABLE,
+        .gpio_acpi_handler = NULL,
+        .cpu = {
+            .package = CPU_PKG_SOCKET1,
+            .block = CPU_BLOCK_NONE,
+            .min_bus = 0,
+            .max_bus = 0,
+            .min_voltage = 0,
+            .max_voltage = 0,
+            .min_multi = 0,
+            .max_multi = 0
+        },
+        .bus_flags = MACHINE_VLB,
         .flags = MACHINE_APM,
         .ram = {
             .min = 1024,
